5930f218096651cf624b36af070db53570d6fe16
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-30 15:40:23 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,14 +12,14 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  @post = Post.new(post_params)
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
 
